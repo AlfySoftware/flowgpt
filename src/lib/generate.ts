2d5c34556f7088542ff1510336b7951c5385a621
Alfy Software <38970899+AlfySoftware@users.noreply.github.com>
2023-06-16 17:09:36 +0800

    Update generate.ts

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -11,7 +11,7 @@ export const generate = async ({ input, selectedTemplate }) => {
     const model = new OpenAI({ temperature: 0.9 });
 
     const template =
-      "{syntax} - {instructions} learn from syntax above and write {template} in mermaid syntax about {input}?";
+      "{syntax} - {instructions} 从上面的语法中学习并写作 {template} 在 mermaid 语法中关于 {input}?";
     const prompt = new PromptTemplate({
       template,
       inputVariables: ["template", "input", "syntax", "instructions"],
@@ -29,10 +29,11 @@ export const generate = async ({ input, selectedTemplate }) => {
       input,
       syntax: syntaxDoc.default,
       instructions: `
-      - use different shapes, colors and also use icons when possible as mentioned in the doc.
-      - strict rules: do not add Note and do not explain the code and do not add any additional text except code, 
-      - do not use 'end' syntax
-      - do not use any parenthesis inside block
+      - 使用不同的形状，颜色，并尽可能使用图标，如文件中提到的.
+      - 严格的规则:不添加注释，不解释代码，不添加除代码以外的任何额外文本, 
+      - 不要使用'end'语法,
+      - 不要在块内使用任何括号,
+      - 用中文进行回复!
       `,
     });
 
